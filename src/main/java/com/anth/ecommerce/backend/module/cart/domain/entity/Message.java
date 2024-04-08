3d11f46e2f99944fa1a597c7e36ea3d191a44e47
Anthony Andrade <andradeanthony95@gmail.com>
2024-04-08 18:00:52 -0500

    use getter and setter(lombok)

--- src/main/java/com/anth/ecommerce/backend/module/cart/domain/entity/Message.java
+++ src/main/java/com/anth/ecommerce/backend/module/cart/domain/entity/Message.java
@@ -1,18 +1,19 @@
 package com.anth.ecommerce.backend.module.cart.domain.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Message {
     private String infoMessage;
 
     public Message(String infoMessage) {
         this.infoMessage = infoMessage;
     }
+    public Message(){
 
-    public String getInfoMessage() {
-        return infoMessage;
     }
 
-    public void setInfoMessage(String infoMessage) {
-        this.infoMessage = infoMessage;
-    }
 
 }
